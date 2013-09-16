dcaaddb69e18e80cbf8322e3b131353da793f199
Anton <wraith@ru8623.(none)>
2013-09-16 18:14:01 +0400

    fix again

--- tasks/json2ngjs.js
+++ tasks/json2ngjs.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
   // compile a template to an angular module
   var compileTemplate = function(moduleName, filepath, quoteChar, indentString) {
 
-    var content = JSON.dumps(grunt.file.read(filepath));
+    var content = JSON.stringify(grunt.file.read(filepath));
     var doubleIndent = indentString + indentString;
 
     var module = 'angular.module(' + quoteChar + moduleName +
